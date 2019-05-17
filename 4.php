04d6f372338c9705e5ea0606ea813fd4b0cebb02
thilinitd <thilini.td@gmail.com>
2019-05-17 09:00:50 +0530

    PHP

---
+++ 4.php
@@ -0,0 +1,50 @@
+<?php
+$x=6;
+$y=9;
+if ($x>$y){
+	echo"{$x} is greater than {$y}";
+}
+if ($x<$y){
+	echo "{$y} is greater than {$x}";
+	}
+	echo"<br/>";
+?>
+<?php
+$x=28;
+$y=25;
+if($x<$y){
+echo "{$y} is greater than {$x}";
+}
+elseif ($x>$y){
+	echo "{$x} is greater than {$y}";
+	}
+	else{
+		echo"{$x} is equal to {$y}";
+	}
+	echo"<br/>";
+?>
+<?php
+$x="3";
+$y=9;
+$z="3";
+if($x==$y){
+	echo "the number is equal";
+}
+if($x!=$y){
+	echo"the number is not equal";echo"<br/>";
+	
+		if($x>$y){
+			echo "x is greater than y";
+		}
+		else{
+			echo "y is greater than x";
+	}
+	echo"<br/>";
+}
+if($x==$z){
+	echo "the number ia equal";echo"<br/>";
+}
+if($x===$z){
+	echo "x is equal to z and they are of the same type";
+}
+?>
